1dcdaed099d1112793a99dea83c2c4a479d5aa1a
Shaikh Saeed <113835995+SaeedShaikh786@users.noreply.github.com>
2024-05-02 21:54:10 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 from mcqgenrator.utils import read_file,get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from mcqgenrator.MCQGenrator import generate_evaluate_chain
-from mcqgenrator.logger import logging
+from MCQ_Generator.mcqgenerator import generate_evaluate_chain
+from MCQ_Generator.logger import logging
 
 #loading json file
 
