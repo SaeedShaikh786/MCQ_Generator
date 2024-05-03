9799ebd397d14f105f4fd8c571001e709a4b36a6
Shaikh Saeed <113835995+SaeedShaikh786@users.noreply.github.com>
2024-05-03 16:48:28 +0530

    Update utils.py

--- src/MCQ_generator/utils.py
+++ src/MCQ_generator/utils.py
@@ -22,7 +22,7 @@ def read_file(file):
     if file.name.endswith('.pdf'):
         try:
             # If the file is a PDF, use PyPDF2 to extract text from each page
-            pdf_reader = PyPDF2.PdfFileReader(file)
+            pdf_reader = PyPDF2.PdfReader(file)
             text = ""
             for page in pdf_reader.pages:
                 text += page.extract_text()
